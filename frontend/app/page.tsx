7156d387162fafaefc82b9e75a62d74b61ca3539
Utpall <uk9507855135@gmail.com>
2026-03-03 14:39:31 +0530

    chore: update frontend dependencies and add new packages

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,15 +1,1 @@
-"use client";
-
-function page() {
-
-  const handlegoole = () => {
-    window.location.href = "http://localhost:8000/api/v1/auth/google"
-  }
-  return (
-    <div className='text-red-500 p-5 rounded-2xl bg-amber-200 m-4 w-20'>Login
-    <button onClick={handlegoole}>Login</button>
-    </div>
-  )
-}
-
-export default page
+export default function Page() {}
