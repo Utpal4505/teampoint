dea1105cdb40a6b7043bd7f5dcd71682e94cbe68
Utpall <uk9507855135@gmail.com>
2026-05-19 20:11:13 +0530

    minimal render test

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,13 @@
-import 'dotenv/config'
-import { createServer } from 'node:http'
-import { app } from './app.js'
-import { initializeSocketServer } from './services/socket.service.js'
+import express from 'express'
+
+const app = express()
 
 const PORT = Number(process.env.PORT) || 3000
 
-const server = createServer(app)
+app.get('/', (_, res) => {
+  res.send('Server works')
+})
 
-initializeSocketServer(server)
-
-server.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server is running on http://localhost:${PORT}`)
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`)
 })
